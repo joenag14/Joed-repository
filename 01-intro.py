8aad9f6d3437de7f185ae4fad3cbd3a666c027e6
joenag14 <156930366+joenag14@users.noreply.github.com>
2024-03-01 15:41:08 -0500

    Add files via upload

---
+++ 01-intro.py
@@ -0,0 +1,3 @@
+print("Hello World")
+print(" repeated word" * 4)# Si a un print, se le agrega un * y un numero despues de cerrar comillas, 
+# la palabra entre comillas se repetira el numero de veces de la palabra, como se podra evidenciar el ejemplo.
