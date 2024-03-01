159883721eca4fb2ed2cc37e0ce8f7262d9ffbfb
joenag14 <156930366+joenag14@users.noreply.github.com>
2024-03-01 15:47:35 -0500

    Add files via upload

---
+++ 03-format-strings.py
@@ -0,0 +1,10 @@
+nombre="Joed"
+apellido="Alvarez"
+nombre_completo=  nombre + " " + apellido
+print (nombre_completo)
+# Esta es la forma de concatenar las variables. 
+nombre="Joed"
+apellido="Alvarez"
+nombre_completo=  f"{nombre} {apellido}"
+print (nombre_completo)
+# Esta es la forma de formatear o dar formato a las variables. 
