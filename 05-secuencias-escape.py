c020c8cf84b8126a692b319816c56ca1df073844
joenag14 <156930366+joenag14@users.noreply.github.com>
2024-03-13 16:04:03 -0500

    Add files via upload

---
+++ 05-secuencias-escape.py
@@ -0,0 +1,12 @@
+curso = 'Ultimate "python"'
+print(curso)
+# para imprimir un mensaje que tenga comillas, habran varias formas, una de ella es poniendo el mensaje global en una comilla simple (alt+39)
+# y la palabra o palabras entre comillas usando comillas dobles.
+
+curso = 'Ultimate "python"'
+print(curso)
+# La segunda manera sera utilizar un back slash \ (alt+92), este se debe agregar antes de las primeras comillas dobles y despues de la palabra
+# o palabras entre comillas,  aunque para este ejemplo el sistema automaticamente lo corrgio.
+
+curso = 'Ultimate \n"python"'
+print(curso)
