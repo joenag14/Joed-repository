f34ec6e7ac7079e52cc79272059d4a891a841223
joenag14 <156930366+joenag14@users.noreply.github.com>
2024-03-14 15:59:29 -0500

    Add files via upload

---
+++ 07-numeros-funciones.py
@@ -0,0 +1,26 @@
+import math  # math es un archivo el cual contiene codigo escrito en python.
+
+print(round(1.3))
+print(
+    round(1.6)
+)  # Round lo que hace es "redondear" el numero al numero mas cercano, por ejemplo, 1.3 esta mas cerca de 1 que de 2 y 1.6 esta mas cerca de 2 que de 1.
+print(
+    abs(-77)
+)  # este va a eliminar el simbolo de un numero si por ejemplo se ingresa un numer negativo, el sistema va a imprimir solo valores positivos.
+print(abs(55))
+
+print(
+    math.ceil(1.1)
+)  # esta propiedad, lo que hace es acercar el numero ingresado entre parentesis al numero entero mas cercano, hacia arriba, en este caso el 2.
+print(
+    math.floor(1.99999)
+)  # hara lo mismo que la propiedad anterior pero en vez de acercar al numero entero siguiente lo hara al anterior.
+print(
+    math.isnan(0)
+)  # Este metodo, arojara un boolean como respuesta, sin embargo, no se puede agregar stings o palabras entre parentesis por que va a ser tomado como un error de sitasix.
+print(
+    math.pow(10, 3)
+)  # sirve para elevar un numero, en este ejemplo se ve 10 elevado a la 3.
+print(math.sqrt(9))  # Sirve para sacar la raiz cuadrada del numero entre parentesis.
+
+# para visualizar todos lo metodos de math, ir a esta pagina... (https://docs.python.org/3/library/math.html) ...
