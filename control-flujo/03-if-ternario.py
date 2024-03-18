29357855f167758d30485da9868e36e26848c633
joenag14 <156930366+joenag14@users.noreply.github.com>
2024-03-18 16:22:56 -0500

    Add files via upload

---
+++ control-flujo/03-if-ternario.py
@@ -0,0 +1,13 @@
+edad = 19
+
+mensaje = "es mayor" if edad > 17 else "es menor"
+
+
+# if edad > 17:
+#    mensaje = " Es mayor"
+
+# else:
+#    mensaje = "Es menor"
+
+print(mensaje)
+# estas son dos maneras diferentes de representar un "if else" el operador ternario seria el que se ve en la linea #3, eso nos ayudaria a ahorrar espacio de lineas en el codigo.
