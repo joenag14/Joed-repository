e83ced007299d6635050308fbfdc884d123513ac
joenag14 <156930366+joenag14@users.noreply.github.com>
2024-04-23 08:40:30 -0500

    Add files via upload

---
+++ control-flujo/05-cadena-comparadores.py
@@ -0,0 +1,6 @@
+edad = 25
+
+if 15 <= edad <= 65:
+    print("puede entrar a la piscina")
+
+    # Para encadenar los comparadores se puede hacer como se evidencia en la parte superior, importante tener en cuenta la ubicacion de los signos.
