5d3fafcc17a65be1033f051221fee8bcf6aa6438
joenag14 <156930366+joenag14@users.noreply.github.com>
2024-05-01 15:32:46 -0500

    Update 07-while.py

--- control-flujo/07-while.py
+++ control-flujo/07-while.py
@@ -2,3 +2,6 @@ numero = 1
 while numero < 100:
     print(numero)
     numero += 2
+
+# En el ejemplo anterior se hace uso de un while, multiplicando el numero 1 declarado en la variable numero* 2
+    # y va a seguir mostrando el numero mientras (while) este menor que 100.
