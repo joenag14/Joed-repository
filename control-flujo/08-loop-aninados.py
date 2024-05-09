b21a175a84b763ee47b2cb8ad6ab87cfa926f5c7
joenag14 <156930366+joenag14@users.noreply.github.com>
2024-05-09 15:59:37 -0500

    Add files via upload

---
+++ control-flujo/08-loop-aninados.py
@@ -0,0 +1,6 @@
+for j in range(3):
+    for k in range(2):
+        print(f"{j}, {k}")
+
+# En este ejemplo, se ejecuta un for dentro de otro, este ejemplo es solo para tener la claro como funcionaria.
+# Nunca es recomendado usar un for dentro de un for ya que eso podria afectar el rendimineto de la aplicacion.
