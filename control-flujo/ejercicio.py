7b670612f6b544b8b3922784a7578ebdf90ae4f2
joenag14 <156930366+joenag14@users.noreply.github.com>
2024-05-11 10:00:28 -0500

    Update ejercicio.py

--- control-flujo/ejercicio.py
+++ control-flujo/ejercicio.py
@@ -6,26 +6,30 @@ n2 = input("ingrese el segundo numero:")
 op = input("ingrese operacion:")
 n1 = int(n1)
 n2 = int(n2)
+
 suma = n1 + n2
 resta = n1 - n2
 multi = n1 * n2
 div = n1 / n2
 
-if op == suma:
+resultado1 = suma
+resultado2 = resta
+resultado3 = multi
+resultado4 = div
+
+if op == "suma":
     print("el resultado de la operacion es")
-    print(suma)
+    print(resultado1)
 
 
-elif op == resta:
+elif op == "resta":
     print("el resultado de la operacion es")
-    print(resta)
+    print(resultado2)
 
-elif op == multi:
+elif op == "multi":
     print("el resultado de la operacion es")
-    print(multi)
+    print(resultado3)
 
-elif op == div:
+elif op == "div":
     print("el resultado de la operacion es")
-    print(div)
-
-resultado= 
+    print(resultado4)
